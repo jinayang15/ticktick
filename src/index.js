0ae2775ced1c757ee6c3f2e915f411894dbd89bd
Jina Yang <jinayang15@gmail.com>
2024-07-12 22:51:59 -0400

    Starting UI layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { Todo, List } from "./class";
 import * as App from "./app";
 import * as Output from "./output";
+import "./reset.css";
+import "./styles.css";
 
 function main() {
   const defaultList = new List("default");
@@ -11,5 +13,3 @@ function main() {
     Output.printListActions();
   }
 }
-
-main();
